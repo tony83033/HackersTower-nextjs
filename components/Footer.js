ea67b6ad680dbd6bcda376a37750077303150e12
Sumit pal <tonystark83033@gmail.com>
2023-06-07 13:50:49 +0530

    add refund

--- components/Footer.js
+++ components/Footer.js
@@ -34,26 +34,19 @@ const Footer = () => {
       <a className="text-gray-500 cursor-pointer " href='https://merchant.razorpay.com/policy/LyNCFirGcMeVKd/terms' target="_blank" rel="noreferrer">
       <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
       Terms & Conditions
-      <a
-        href="https://twitter.com/knyttneve"
-        className="text-gray-600 ml-1"
-        rel="noopener noreferrer"
-        target="_blank"
-      />
+     
     </p>
 
       </a>
-      <a className="ml-3 text-gray-500">
-        <svg
-          fill="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          className="w-5 h-5"
-          viewbox="0 0 24 24"
-        >
-          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-        </svg>
+
+
+
+      <a className="ml-3 text-gray-500" href='https://merchant.razorpay.com/policy/LyNCFirGcMeVKd/refund'>
+         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+         Cancellation & Refund Policy
+      
+    </p>
+
       </a>
       <a className="ml-3 text-gray-500 cursor-pointer"  href='https://www.instagram.com/hackerstowerofficial/' target="_blank" rel="noreferrer">
         <svg
